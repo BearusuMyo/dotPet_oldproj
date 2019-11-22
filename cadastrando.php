49c57b349b76259145ad29bfba476fe4efb9e604
Hiago Cardoso de Melo <31222857+BearusuMyo@users.noreply.github.com>
2019-11-21 20:46:40 -0400

    Version 3.4.1
    Redo wrong things.

--- cadastrando.php
+++ cadastrando.php
@@ -13,48 +13,16 @@
 <?php
 
       require("conexao.php");
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      if(empty($_POST["tx_nome"])){
-      	echo "Você precisa inserir seu nome completo!";
-      }
-      else
-    {
+
       $tx_nome = $_POST["tx_nome"];
-    }
-	  if(empty($_POST["tx_email"])){  
-	  echo "Você precisa informar um e-mail!";
-	}
-	  else{ 
-      $tx_email = input::email($_POST["tx_email"]);
-    }
-      if (empty($_POST["tx_datanasc"])){
-      	echo "Por favor informe uma data de nascimento válida!";
-      }
-      else{
+      $tx_email = ($_POST["tx_email"]);
       $tx_datanasc = $_POST["tx_datanasc"];
-      }
-      if (empty($_POST["tx_ende"])) {
-      	echo "Insira um endereço válido pelo amor de Jeová.";
-      }else{
-      	 $tx_ende = $_POST["tx_ende"];
-      }
-      if (empty(($_POST["tx_telefone"]){
-      	echo "O campo Telefone não deve estar vazio. Favor informe um número válido!";
-      } else{
+      $tx_ende = $_POST["tx_ende"];
       $tx_telefone = $_POST["tx_telefone"];
-      }
-      if (empty($_POST["tx_user"])){
-		echo "Por favor, insira um usuário!";      	
-      } else {
       $tx_user = $_POST["tx_user"];
-  	  }
-  	  if (empty($_POST["tx_senha"])){
-  	  	echo "Por favor insira uma senha";
-  	  }
       $tx_senha = $_POST["tx_senha"];
       $tx_passconf = $_POST["tx_passconf"];
 
-}
 
 
       $sqlinsert = "INSERT INTO cadastro (tx_nome,tx_email,tx_datanasc,tx_ende,tx_telefone,tx_user,tx_senha,tx_passconf) VALUES ('$tx_nome','$tx_email','$tx_datanasc','$tx_ende','$tx_telefone','$tx_user','$tx_senha','$tx_passconf')";
